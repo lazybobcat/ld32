2b049545114850d20dfa749eeb013ef89e220f47
Lo-X <loic.boutter@gmail.com>
2015-04-18 09:11:59 +0200

    Removed assets from src

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#include "application.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const unsigned int width = 1280, height = width * 9/16;
+    try {
+        Application game(width, height, "UnikOrn");
+        game.run();
+    } catch(std::exception& e) {
+        std::cerr << "Game ended unexpectedely : " << std::endl;
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
-
